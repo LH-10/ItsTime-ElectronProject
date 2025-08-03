d3963cb22e8bacd03dfb15d1a9c9716889678e57
LH-10 <lalit1652h@gmail.com>
2025-08-03 16:15:51 +0530

    restructured timercard logic added more functionality

--- The-app/src/React/Components/TimerCard.tsx
+++ The-app/src/React/Components/TimerCard.tsx
@@ -1,102 +1,168 @@
 import { useNavigate } from "react-router-dom"
-import { useEffect ,useState} from "react"
+import { useEffect, useRef, useState } from "react"
 import { Trash2, RotateCcw, Play, Pause } from 'lucide-react';
 
 const TimerCard = () => {
-    const navigate = useNavigate()
-    const [percentage,setPercentage]=useState<number>(1)
-    const [fill,setFill]=useState<number>()
-    useEffect(() => {
-        const unsub = window.electron.subscribeStatistics((stats) => {
-            console.log(stats)
-          });
-          // setInterval(()=>{
-          //   setPercentage((prevPercentage)=> (prevPercentage+10)%100)
-          // },1000)
-        return unsub;
-    }, [])
-    useEffect(()=>{
-      const filllength=percentageTofillLength(percentage)
-      setFill(filllength);
-    },[percentage])
-    const circumference = 2 * Math.PI * 45; //283
-    const percentageTofillLength=(percentage:number)=>{
-      
-      const filledLength = (percentage)/100  * circumference;
-      return filledLength;
+  const navigate = useNavigate()
+  const orignaltime = 120
+  const [timeLeft, setTimeLeft] = useState(120); // 25 minutes
+  const [percentage, setPercentage] = useState<number>(0)
+  const [fill, setFill] = useState<number>()
+  const [countDown, setCountDown] = useState()
+  const [start, setStart] = useState(false)
+  
+  const intervalref = useRef<any>(null)
+  
+  const circumference = 2 * Math.PI * 45; //283
+  
+  // const [timeLeft,setTimeLeft]=useState({
+  //   minutes:25,
+  //   seconds:12,
+  // })
+
+
+  const startPauseHandler = () => {
+    setStart(start => !start)
+  }
+
+  const timerHandler=()=>{
+   if (start == true) {
+      setPercentage(100)
     }
-    // const [timeLeft,setTimeLeft]=useState({
-    //   minutes:25,
-    //   seconds:12,
-    // })
-    const [timeLeft, setTimeLeft] = useState(25*60); // 25 minutes
+    else {
+      if (intervalref.current)
+        clearInterval(intervalref.current)
+      return
+    }
+    intervalref.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        const newTimeLeft = timeLeft - 1
+        return newTimeLeft
+      }
+      )
+      console.log(percentage)
+    }, 1000)
+    
+    return () => {
+      if (intervalref.current) {
+        clearInterval(intervalref.current);
+      }
+    };
+  }
+  
+  const handleProgressBar=()=>{
+    const currenttimeLeft = timeLeft / orignaltime * 100
+    setPercentage(
+      currenttimeLeft
+    )
+  }
+  
   const formatTime = (seconds: number) => {
     const m = Math.floor(seconds / 60);
     const s = seconds % 60;
     return `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
   };
-  const [countDown,setCountDown]=useState()
-  const [start,setStart]=useState(false)
-  return (
-      <div className="bg-white shadow-lg rounded-xl p-6 w-64 text-center relative">
-        {/*  Icons */}
-        <div className="flex justify-between mb-6">
-          <button title="Delete" className="p-2 rounded bg-gray-50 hover:bg-gray-300">
-            <Trash2  className="w-5 h-5 text-gray-600" />
-            {/* Del Icon */}
-          </button>
-          <button  title="Reset" className="p-2   bg-gray-50 rounded hover:bg-gray-300">
-            <RotateCcw className="w-5 h-5 text-gray-500 " />
-            {/* Reset icon */}
-          </button>
-        </div>
 
-          {/* Circle */}
-  {/* Circle */}
-<div className="flex items-center justify-center mb-6">
-  <div className="relative w-44 h-44 rounded-full bg-gray-200 flex items-center shadow-md justify-center">
-    {/* Circular loading bar */}
-    <div className="absolute inset-0 ">
-      <svg
-        className="w-full h-full duration-100"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="50"
-          cy="50"
-          r="45"
-          fill="none"
-          className="stroke-current text-blue-600 duration-150 dark:text-blue-500"
-          strokeWidth="9.5"
-          strokeDasharray={`${fill} ${circumference}`}
-          strokeLinecap="butt"
-          transform="rotate(90 50 50)"
-        />
-      </svg>
-    </div>
-
-    {/* Inner circle with time */}
-    <div className="w-40 h-40 shadow-[inset_0_2px_4px_rgba(0,0,0,0.2)] rounded-full flex items-center justify-center bg-white z-10">
-      <span className="text-3xl font-bold text-gray-800">
-        {formatTime(timeLeft)}
-      </span>
-    </div>
+  const resetOrignalTime = () => {
+    setStart(false)
+    setTimeLeft(orignaltime)
+  }
+  
+  useEffect(() => {
+    const unsub = window.electron.subscribeStatistics((stats) => {
+      // console.log(stats)
+    });
+    // setInterval(()=>{
+      //   setPercentage((prevPercentage)=> (prevPercentage+10)%100)
+      // },1000)
+      return unsub;
+    }, [])
     
-  </div>
-</div>
+    const percentageTofillLength = (percentage: number) => {
+  
+      const filledLength = (percentage) / 100 * circumference;
+      return filledLength;
+    }
+    
+  useEffect(//for filling circular progress bar
+    () => {
+    const fillLength = percentageTofillLength(percentage)
+    setFill(fillLength);
+    
+  }, [percentage])
 
 
-        {/* Start Btn */}
-        {start?(
-        <button onClick={()=>{setStart(!start)}} className=" bg-gray-600  hover:bg-gray-700  text-white rounded-md font-medium px-2 py-2  flex items-center justify-center gap-2 w-full">
+  useEffect(timerHandler, [start])
+  useEffect(()=>{
+    handleProgressBar()
+    if(timeLeft<=0){
+      clearInterval(intervalref.current);
+      console.log("its time")
+      setTimeout(resetOrignalTime,1000)
+    }
+  },[timeLeft])
+
+
+  return (
+    <div className="bg-white shadow-lg rounded-xl p-6 w-64 text-center relative">
+      {/*  Icons */}
+      <div className="flex justify-between mb-6">
+        <button title="Delete" className="p-2 rounded bg-gray-50 hover:bg-gray-300">
+          <Trash2 className="w-5 h-5 text-gray-600" />
+          {/* Del Icon */}
+        </button>
+        <button title="Reset" onClick={resetOrignalTime} className="p-2   bg-gray-50 rounded hover:bg-gray-300">
+          <RotateCcw className="w-5 h-5 text-gray-500 " />
+          {/* Reset icon */}
+        </button>
+      </div>
+
+      {/* Circle */}
+      {/* Circle */}
+      <div className="flex items-center justify-center mb-6">
+        <div className="relative w-44 h-44 rounded-full bg-gray-200 flex items-center shadow-md justify-center">
+          {/* Circular loading bar */}
+          <div className="absolute inset-0 ">
+            <svg
+              className="w-full h-full duration-200"
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="50"
+                cy="50"
+                r="45"
+                fill="none"
+                className="stroke-current text-blue-600 duration-1000 dark:text-blue-500"
+                strokeWidth="9.5"
+                strokeDasharray={`${fill} ${circumference}`}
+                strokeLinecap="butt"
+                transform="rotate(90 50 50)"
+              />
+            </svg>
+          </div>
+
+          {/* Inner circle with time */}
+          <div className="w-40 h-40 shadow-[inset_0_2px_4px_rgba(0,0,0,0.2)] rounded-full flex items-center justify-center bg-white z-10">
+            <span className="text-3xl font-bold text-gray-800">
+              {formatTime(timeLeft)}
+            </span>
+          </div>
+
+        </div>
+      </div>
+
+
+      {/* Start Btn */}
+      {start ? (
+        <button onClick={startPauseHandler} className=" bg-gray-600  hover:bg-gray-700  text-white rounded-md font-medium px-2 py-2  flex items-center justify-center gap-2 w-full">
           <Pause className="w-4 h-4  fill-white " />
           <p className="pb-1">Pause</p>
-        </button>):(<button onClick={()=>{setStart(!start)}} className="group bg-blue-600  hover:bg-blue-700  text-white rounded-md font-medium px-2 py-2  flex items-center justify-center gap-2 w-full">
+        </button>) : (<button onClick={startPauseHandler} className="group bg-blue-600  hover:bg-blue-700  text-white rounded-md font-medium px-2 py-2  flex items-center justify-center gap-2 w-full">
           <Play className="w-4 h-4  group-hover:fill-white " />
           <p className="pb-1">Start</p>
         </button>)}
-      </div>
+    </div>
   );
 }
 
