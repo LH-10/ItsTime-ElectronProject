1662c7a40a4f758ec9f2943906bb533c234776a3
LH-10 <lalit1652h@gmail.com>
2025-12-24 14:21:52 +0530

    dx:add error handling in db init

--- The-app/src/database/sqldb.ts
+++ The-app/src/database/sqldb.ts
@@ -1,12 +1,19 @@
 import database, { Database } from "better-sqlite3";
 import path from "node:path"
 import {app} from "electron"
+import { throws } from "node:assert";
 
 let db:Database;
 function initDb(){
 
     const dbPath=path.join(app.getPath('userData'),'dbtest.db');
-    db=new database(dbPath)   
+    try{
+        db=new database(dbPath)   
+    }
+    catch(err){
+        console.log(err,"DB init issue")
+        throw err
+    }
 }
 
 export {db,initDb}
